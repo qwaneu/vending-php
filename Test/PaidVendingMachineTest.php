9a8b4e053d789e6675ee1f842253328410f71b67
Willem van den Ende <willem@qwan.eu>
2024-10-07 22:24:19 +0100

    Refactor: setup not yet needed

--- Test/PaidVendingMachineTest.php
+++ Test/PaidVendingMachineTest.php
@@ -17,7 +17,6 @@ class PaidVendingMachineTest extends HamcrestTestCase
     {
         parent::setUp();
         $this->machine = new VendingMachine();
-       $this->machine->configure(Choice::Cola, Can::Coke);
     }
 
     public function testMachineWithNoPaymentDeliversNothing() {
