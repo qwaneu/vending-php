48187e87d5ce4c68a09fb1ff66fee150755c2759
Willem van den Ende <willem@qwan.eu>
2024-10-07 21:56:30 +0100

    First failing test - starting point for exercise

--- Test/VendingMachineTest.php
+++ Test/VendingMachineTest.php
@@ -5,33 +5,12 @@ namespace Test;
 use App\Choice;
 use App\Can;
 use App\VendingMachine;
-use PHPUnit\Metadata\CoversNothing;
-
 
 class VendingMachineTest extends HamcrestTestCase
 {
-    private VendingMachine $machine;
-
-    public function setUp(): void {
-        parent::setUp();
-        $this->machine = new VendingMachine();
-
-        $this->machine->configure(Choice::FizzyOrange, Can::Fanta);
-        $this->machine->configure(Choice::Cola, Can::Coke);
-    }
-    public function testMachineDeliversNothingForUnconfiguredChoice()
+    public function testFirst()
     {
-        $choice = Choice::Beer;
-        assertThat($this->machine->deliver($choice)->value, equalTo(Can::Nothing->value));
-    }
-
-    public function testMachineConfiguredWithCokeDeliversCoke() {
-        assertThat($this->machine->deliver(Choice::Cola)->value, equalTo(Can::Coke->value));
-    }
-
-    public function testMachineConfiguredWithFantaAndCokeDeliversFanta()
-    {
-        assertThat($this->machine->deliver(Choice::FizzyOrange)->value, equalTo("Fanta"));
+        assertThat(true, equalTo(false));
     }
 
 }
