6241e05b38a6491d74d176ebd4b67ee5f434eac9
Willem van den Ende <willem@qwan.eu>
2024-10-07 19:33:09 +0100

    Refactor: extract setup

--- Test/VendingMachineTest.php
+++ Test/VendingMachineTest.php
@@ -9,18 +9,27 @@ use App\VendingMachine;
 
 class VendingMachineTest extends HamcrestTestCase
 {
+    public function setUp(): void {
+        parent::setUp();
+        $this->machine = new VendingMachine();
+    }
     public function testChoicelessMachineDeliversNothing()
     {
-        $machine = new VendingMachine();
         $choice = Choice::Cola->value;
-        assertThat($machine->deliver($choice)->value, equalTo("Nothing"));
+        assertThat($this->machine->deliver($choice)->value, equalTo("Nothing"));
     }
 
     public function testMachineConfiguredWithCokeDeliversCoke() {
-        $machine = new VendingMachine();
-        $machine->configure(Choice: Choice::Cola, drink: Can::Coke);
-        assertThat($machine->deliver("Cola")->value, equalTo("Coke"));
+        $this->machine->configure(Choice: Choice::Cola, drink: Can::Coke);
+        assertThat($this->machine->deliver("Cola")->value, equalTo("Coke"));
     }
+
+    /*
+    public function testMachineConfiguredWithFantaAndCokeDeliversFanta()
+    {
+
+        assertThat($machine->deliver("FizzyOrange")->value, equalTo("Fanta"));
+    }*/
 
 
 
